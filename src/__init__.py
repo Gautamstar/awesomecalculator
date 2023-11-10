79c674f7b87d75c4e8a632ae7a7a8cf891d2a6f0
Gautam <gautamsingh2189@gmail.com>
2023-11-10 16:37:38 -0500

    Made changes.

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from core.py import _init_
+from core.py import Add
+from core.py import Substract
+from core.py import Multiply
+from core.py import Divide
--- src/setup.py
+++ src/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dedreira-awesomecalculator", # Replace with your own username
-    version= "1.0.0",
+    name="Cecil21899", # Replace with your own username
+    version= "1.0.0",    #pypi-AgENdGVzdC5weXBpLm9yZwIkZmRmODgzNDYtMGEyNS00NTA2LTg0N2EtZmY1YjM0ZGJjNDIzAAIqWzMsImIxOWRjYWZhLThiNTEtNDUxNC1hZjlkLTBkZGMzNDk3MjdlNCJdAAAGINirtyV9A_w_p4a02EzunRWd2BDgOPACEsTJSKJUkqyH
     author="Daniel Edreira",
     author_email="dedreira@github.com",
     description="An awesome calculator package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dedreira/awesomecalculator",
+    url="https://github.com/Gautamstar/awesomecalculator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
